af9fd4bf2e5f0ca0977d79b2c2e0c6d6bcca830c
Joakim <joakikr@gmail.com>
2014-01-02 00:41:55 +0100

    Added minor fix in ui/BotPlayer.java

--- ui/BotPlayer.java
+++ ui/BotPlayer.java
@@ -1,6 +1,8 @@
 package ui;
 
 public class BotPlayer extends Player{
+	public static final int NUM_BOT_NAMES = 7;
+	public static final String[] botNames = {"Ola", "Jack", "Jon", "Master", "Vanessa", "Christy", "Emelie", "Botonator"};
 	public BotPlayer(String name) {
 		super(name);
 	}
