d4d4b01f6c1b29676f637fc4f939b6047c93683d
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2014-01-01 18:44:23 +0100

    Added skeleton for game->hasStarted game->playerJoin methods

--- game/Game.java
+++ game/Game.java
@@ -9,16 +9,32 @@ import ui.Player;
  */
 public class Game {
 	private ArrayList<Player> players;
+	private boolean started;
 	
 	public Game() {
+		started = false;
 		players = new ArrayList<Player>();
 	}
 	
+	/**
+	 * Join a player. This can only be done if the game
+	 * has not already started
+	 * @param player
+	 */
 	public void playerJoin(Player player) {
+		if (hasStarted()) return;
 		players.add(player);
 	}
 	
+	public boolean hasStarted() {
+		return started;
+	}
+	
+	/**
+	 * Start the game
+	 */
 	public void start() {
-		
+		if (hasStarted()) return;
+		started = true;
 	}
 }
--- game/RemoteGame.java
+++ game/RemoteGame.java
@@ -1,10 +1,18 @@
 package game;
 
+import java.util.ArrayList;
+
+import ui.Player;
+
 /**
  * Creates a new game that joins a remote game
  */
 public class RemoteGame extends Game {
 	public RemoteGame(String ip) {
 		
+	}
+	
+	public void playerJoin(Player player) {
+		super.playerJoin(player);
 	}
 }
