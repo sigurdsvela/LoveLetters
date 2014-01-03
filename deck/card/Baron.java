7e17d2f44e8f97e759daf297898f4b0894339b3c
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2014-01-03 04:47:59 +0100

    Implemented dummy version of CardRule for Baron

--- deck/card/Baron.java
+++ deck/card/Baron.java
@@ -1,7 +1,46 @@
 package deck.card;
 
+import game.Game;
+import player.Player;
+import deck.card.rule.CardRule;
+
 public class Baron extends Card {
 
+	public Baron() {
+		this.addRule(new CardRule() {
+			Player winningPlayer;
+			Player versusPlayer;
+			Player cardOwner;
+			
+			@Override
+			public boolean condition(Game game, Player cardOwner) {
+				this.cardOwner = cardOwner;
+				return true;
+			}
+
+			@Override
+			public void run(Game game, Player cardOwner) {
+				cardOwner.askPlayerForCard("Who do you want to battle?");
+			}
+
+			@Override
+			public int when() {
+				return CardRule.ON_PLAY;
+			}
+
+			@Override
+			public String message() {
+				return null;
+			}
+
+			@Override
+			public String description() {
+				return null;
+			}
+			
+		});
+	}
+	
 	@Override
 	public byte getDistance() {
 		// TODO Auto-generated method stub
