8784222344e071b9d9cb2645947f2dc62c2626fa
Joakim <joakikr@gmail.com>
2014-01-03 20:13:52 +0100

    Implemented askPlayerForPlayer, askPlayerForCard methods in player/BotPlayer.java

--- player/BotPlayer.java
+++ player/BotPlayer.java
@@ -1,7 +1,8 @@
 package player;
 
-import deck.card.Card;
 import game.Game;
+import view.View;
+import deck.card.Card;
 
 public class BotPlayer extends Player{
 	public static final int NUM_BOT_NAMES = 7;
@@ -9,31 +10,59 @@ public class BotPlayer extends Player{
 	public BotPlayer(String name, Game game) {
 		super(name, game);
 	}
+	
 	@Override
 	public Card playCard() {
-		int cardToPlayIndex = (int)Math.random()*cards.size();
+		int cardToPlayIndex = (int) (Math.random() * cards.size());
 		return playCard(cardToPlayIndex);
 	}
+	
 	@Override
 	public void drawCard(Card card) {
 		cards.add(card);
 	}
+	
 	@Override
 	public Player askPlayerForPlayer(String message) {
-		return null;
+		View view = game.getView();
+		view.setInformation(message);
+		Player playerToChoose;
+		Player[] playersInRound = game.getPlayersInThisRound();
+		
+		while (true) {
+			int playerIndex = (int) (Math.random() * playersInRound.length);
+			playerToChoose = playersInRound[playerIndex];
+			if (playerToChoose.getName().compareTo(getName()) != 0) {
+				break;
+			}
+		}
+		view.setInformation(getName() + " chose " + playerToChoose.getName() + ".\n");
+		return playerToChoose;
 	}
+	
 	@Override
 	public Player askPlayerForPlayer() {
-		return null;
+		return askPlayerForPlayer("Choose a player:");
 	}
+	
 	@Override
 	public Card askPlayerForCard(String message) {
-		return null;
+		View view = game.getView();
+		view.setInformation(message);
+		Card cardToChoose;
+		Card[] cardsToChooseFrom = game.getDeck().getCardTypes();
+	
+		int cardIndex = (int) (Math.random() * cardsToChooseFrom.length);
+		cardToChoose = cardsToChooseFrom[cardIndex];
+		view.setInformation(getName() + " chose " + cardToChoose.getName() + ".\n");
+		return cardToChoose;
 	}
+	
 	@Override
 	public Card askPlayerForCard() {
-		return null;
+		return askPlayerForCard("Choose a card:");
 	}
+	
 	@Override
 	public void showCards() {}
 }
