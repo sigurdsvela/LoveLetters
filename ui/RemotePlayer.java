2aa04e56df5acb0d95b6969035ada2a0aa85fbe2
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2014-01-03 01:00:46 +0100

    Added correct call signature for RemotePlayer constructor

--- ui/RemotePlayer.java
+++ ui/RemotePlayer.java
@@ -1,12 +1,14 @@
 package ui;
 
+import game.Game;
+
 /**
  * Creates a new remote player
  * This servers as a representation of the remote player.
  * This is the class that handles the waiting for the response of a player etc.
  */
 public class RemotePlayer extends Player{
-	public RemotePlayer(String name) {
-		super(name);
+	public RemotePlayer(String name, Game game) {
+		super(name, game);
 	}
 }
