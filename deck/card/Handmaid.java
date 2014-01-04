8af48b0383ceddb357ecc9836be960fc9c48c352
Joakim <joakikr@gmail.com>
2014-01-04 18:47:53 +0100

    Implemented deck/card/Handmaid.java

--- deck/card/Handmaid.java
+++ deck/card/Handmaid.java
@@ -1,16 +1,44 @@
 package deck.card;
 
+import game.Game;
+import player.Player;
+import deck.card.rule.CardRule;
+
 public class Handmaid extends Card {
 
+	public Handmaid() {
+		this.addRule(new CardRule() {
+			
+			@Override
+			public boolean condition(Game game, Player cardOwner) {
+				return true;
+			}
+			
+			@Override
+			public void run(Game game, Player cardOwner) {
+				// Protect the player!
+				cardOwner.setIsPlayerProtected(true);
+			}
+
+			@Override
+			public int when() {
+				return CardRule.ON_PLAY;
+			}
+			
+			@Override
+			public String description() {
+				return "When handmaid is played, the player who played her is protected until their next turn.";
+			}
+		});
+	}
+	
 	@Override
 	public byte getDistance() {
-		// TODO Auto-generated method stub
 		return 4;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return "Handmaid";
 	}
 
