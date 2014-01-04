802d3a09f36d83fce60d34f6dfc618f6935d7997
Joakim <joakikr@gmail.com>
2014-01-04 01:43:43 +0100

    Implemented deck/card/Prince.java

--- deck/card/Prince.java
+++ deck/card/Prince.java
@@ -1,16 +1,59 @@
 package deck.card;
 
+import game.Game;
+import player.Player;
+import deck.card.rule.CardRule;
+
 public class Prince extends Card {
 
+	public Prince() {
+		this.addRule(new CardRule() {
+			
+			@Override
+			public boolean condition(Game game, Player cardOwner) {
+				return true;
+			}
+			
+			@Override
+			public void run(Game game, Player cardOwner) {
+				Player affectedPlayer = cardOwner.askPlayerForPlayer("Which player do you want to discard their hand?");
+				game.getView().setInformation(affectedPlayer.getName() + " must discard their hand:");
+				affectedPlayer.showCards(false);
+				
+				// Discard cards in hand
+				while(affectedPlayer.discardCard(0) != null) affectedPlayer.discardCard(0);
+				
+				// Draw a card from deck or the removed card at start if deck is empty
+				Card toBeDrawn;
+				if (game.getDeck().peek() == null) {
+					toBeDrawn = game.getRemovedAtStart();
+				} else {
+					toBeDrawn = game.getDeck().draw();
+				}
+				
+				affectedPlayer.drawCard(toBeDrawn);
+			}
+			
+			@Override
+			public int when() {
+				return CardRule.ON_PLAY;
+			}
+
+			@Override
+			public String description() {
+				return "When you play prince, you may choose a player and that player must discard their hand.";
+			}
+
+		});
+	}
+	
 	@Override
 	public byte getDistance() {
-		// TODO Auto-generated method stub
 		return 5;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return "Prince";
 	}
 
