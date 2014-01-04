b0605afe5eb7bb5c2d124ab594c6a58bc6d8eede
Joakim <joakikr@gmail.com>
2014-01-04 15:13:48 +0100

    Deck now support Countess, Princess

--- deck/Deck.java
+++ deck/Deck.java
@@ -4,14 +4,16 @@ import java.util.LinkedList;
 
 import deck.card.Baron;
 import deck.card.Card;
+import deck.card.Countess;
 import deck.card.Guard;
 import deck.card.King;
 import deck.card.Priest;
 import deck.card.Prince;
+import deck.card.Princess;
 
 public class Deck {
 	private LinkedList<Card> cards;
-	private final Card[] typesOfCards = { new Guard(), new Priest(), new Baron(), new Prince(), new King() };
+	private final Card[] typesOfCards = { new Guard(), new Priest(), new Baron(), new Prince(), new King(), new Countess(), new Princess() };
 	
 	public Deck() {
 		//Initialize the card array
@@ -30,8 +32,8 @@ public class Deck {
 		cards.add(new deck.card.Prince());
 		cards.add(new deck.card.Prince());
 		cards.add(new deck.card.King());
-//		cards.add(new deck.card.Countess());
-//		cards.add(new deck.card.Princess());
+		cards.add(new deck.card.Countess());
+		cards.add(new deck.card.Princess());
 	}
 	
 	/**
