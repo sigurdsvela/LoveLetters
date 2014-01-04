f4e05bcb56d7a0d438e2d99580a923ae0383cb82
Joakim <joakikr@gmail.com>
2014-01-04 14:17:44 +0100

    Implemented Princess in deck/card/Princess.java

--- deck/card/Princess.java
+++ deck/card/Princess.java
@@ -1,16 +1,48 @@
 package deck.card;
 
+import game.Game;
+import player.Player;
+import deck.card.rule.CardRule;
+
 public class Princess extends Card {
 
+	public Princess() {
+		this.addRule(new CardRule() {
+			Player cardOwner;
+			
+			@Override
+			public boolean condition(Game game, Player cardOwner) {
+				this.cardOwner = cardOwner;
+				return true;
+			}
+			
+			@Override
+			public void run(Game game, Player cardOwner) {
+				cardOwner.setIsPlayerInThisRound(false);
+			}
+			
+			@Override
+			public int when() {
+				return CardRule.ON_PLAY | CardRule.ON_DISCARD;
+			}
+			@Override
+			public String description() {
+				return "If you either play Princess or is forced to discard it, you're out of the round.";
+			}
+			
+			public String message() {
+				return cardOwner.getName() + " put down " + getName() + " and is therefore out of round.";
+			}
+		});
+	}
+	
 	@Override
 	public byte getDistance() {
-		// TODO Auto-generated method stub
 		return 8;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return "Princess";
 	}
 	
