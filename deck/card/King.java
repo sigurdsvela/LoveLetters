3391b8ec5d144c571a82908b3ec7d8ee1885829c
Joakim <joakikr@gmail.com>
2014-01-04 02:23:09 +0100

    Implemented deck/card/King.java

--- deck/card/King.java
+++ deck/card/King.java
@@ -1,16 +1,64 @@
 package deck.card;
 
+import game.Game;
+import player.Player;
+import deck.card.rule.CardRule;
+
 public class King extends Card {
 
+	public King() {
+		this.addRule(new CardRule() {
+			
+			@Override
+			public boolean condition(Game game, Player cardOwner) {
+				return true;
+			}
+			
+			@Override
+			public void run(Game game, Player cardOwner) {
+				Player affectedPlayer = cardOwner.askPlayerForPlayer("Which player do you want to switch hand with?");
+				
+				// Do the switch of hands through two loops
+				Card[] tmpHand = cardOwner.getCards();
+				
+				cardOwner.emptyHand(false);
+				for (Card c : affectedPlayer.getCards()) {
+					cardOwner.addCard(c);
+				}
+				
+				affectedPlayer.emptyHand(false);
+				for (Card c : tmpHand) {
+					affectedPlayer.addCard(c);
+				}
+				
+				// "Reveal the cards"
+				game.getView().setInformation(cardOwner.getName() + "'s new hand:");
+				cardOwner.showCards(true);
+				game.getView().setInformation(affectedPlayer.getName() + "'s new hand:");
+				affectedPlayer.showCards(true);
+				
+			}
+			
+			@Override
+			public int when() {
+				return CardRule.ON_PLAY;
+			}
+			
+			@Override
+			public String description() {
+				return "When you play king, switch hand with another player of your choice.";
+			}
+
+		});
+	}
+	
 	@Override
 	public byte getDistance() {
-		// TODO Auto-generated method stub
 		return 6;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return "King";
 	}
 	
