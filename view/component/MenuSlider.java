97a1e18898977aafefc83e32b0bc821d4a93f33a
Joakim <joakikr@gmail.com>
2014-01-08 14:52:57 +0100

    Implemented the MenuSlider in view/component/MenuSlider.java

--- view/component/MenuSlider.java
+++ view/component/MenuSlider.java
@@ -1,9 +1,48 @@
 package view.component;
 
-import javax.swing.JSlider;
+import java.awt.BorderLayout;
 
-public class MenuSlider extends JSlider{
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class MenuSlider extends JPanel{
+	private JLabel label;
+	private JSlider slider;
+	
 	public MenuSlider() {
-		
+		this(1, 3, 2);
 	}
+	
+	public MenuSlider(int min, int max, int defaultValue) {
+		setLayout(new BorderLayout());
+		
+		// Create the Slider for the MenuSlider
+		slider = new JSlider();
+		slider.setMinimum(1);
+		slider.setMaximum(3);
+		slider.setValue(defaultValue);
+		slider.addChangeListener(new ChangeListener() {
+			
+			@Override
+			public void stateChanged(ChangeEvent ce) {
+				JSlider slider = (JSlider)ce.getSource();
+				label.setText("Number of opponents: " + slider.getValue());
+			}
+		});
+		
+		// Create the Label that displays the value for the slider
+		label = new JLabel("Number of opponents: " + slider.getValue());
+		
+		// Add the components to the panel
+		add(slider, BorderLayout.CENTER);
+		add(label, BorderLayout.EAST);
+	}
+	
+	public int getValue() {
+		return slider.getValue();
+	}
+
 }
