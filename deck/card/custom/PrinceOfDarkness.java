54df08640d4b7ff85975ce53817aca55dc754afe
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2014-01-08 13:26:24 +0100

    Implemented skeleton for Prince Of Darkness

---
+++ deck/card/custom/PrinceOfDarkness.java
@@ -0,0 +1,50 @@
+package deck.card.custom;
+
+import game.Game;
+import player.Player;
+import deck.card.Card;
+import deck.card.rule.CardRule;
+
+public class PrinceOfDarkness extends Card{
+	
+	public PrinceOfDarkness() {
+		addRule(new CardRule() {
+			
+			@Override
+			public int when() {
+				return CardRule.ON_PLAY;
+			}
+			
+			@Override
+			public void run(Game game, Player cardOwner) {
+				Player[] players = game.getPlayersInThisRound();
+				for (Player player : players) {
+					for (Player p : players) {
+						//player.showCardsOf(p)
+					}
+				}
+			}
+			
+			@Override
+			public String description() {
+				return "When you play Prince of Darkness. Every player, except you, has to show their card to every player except you.";
+			}
+			
+			@Override
+			public boolean condition(Game game, Player cardOwner) {
+				return true;
+			}
+		});
+	}
+	
+	@Override
+	public byte getDistance() {
+		return 5;
+	}
+
+	@Override
+	public String getName() {
+		return null;
+	}
+	
+}
